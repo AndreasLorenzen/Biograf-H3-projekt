8f51efe1456345d3bcef4ea5268c9ccb48979773
AndreasLorenzen <andreas.141106@gmail.com>
2026-09-21 19:36:28 +0200

    Dobbel Databasecontext fixed

--- Cinema2026.API/DatabaseContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
-{
-    public DbSet<Cinema2026.API.Models.Person1> Person1 { get; set; } = default!;
-}
